089be43c5188c91e80d524f2c99f0e8773e5a9f6
Rylazuli <102565930+RAlynne@users.noreply.github.com>
2022-04-16 19:10:42 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Name/Nama: Rayner Arlyn Chanarly\n')
-    res.send('NIM: 52019027\n')
-    res.send('Jurusan: Teknik Informatika\n')
-    res.send('Mata Kuliah: Cloud Computing\n')
+    res.send('Nama: Rayner Arlyn Chanarly, NIM: 52019027, Jurusan: Teknik Informatika, Mata Kuliah: Cloud Computing\n')
 })
 
 app.listen(PORT, HOST)
